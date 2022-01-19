9a06aec876f35c1b8a83b0e210ce6c44f6f7b388
snagatas <92005197+snagatas@users.noreply.github.com>
2022-01-19 20:24:15 +0900

    Update Fportfolio.R

--- Fportfolio.R
+++ Fportfolio.R
@@ -7,6 +7,19 @@ portF <- function(w,mr,mV) {
 }
 
 
+#ポートフォリのリターンを計算する関数
+Return <- function(weight, return.expected){
+  sum(weight * return.expected)
+}
+
+
+#ポートフォリのリスクを計算する関数
+Risk <- function(weight, covariance.matrix){
+  weight.matrix <- matrix(weight, nrow = length(weight))
+  (t(weight.matrix) %*% covariance.matrix %*% weight.matrix)^0.5 %>% 
+    as.numeric()
+}
+
 
 
 
